0e683594f5cff0a6d33f832bfe2dfa2f80028606
NaytonWilkerson <naytonwilkerson78@gmail.com>
2018-12-03 20:45:59 -0200

    Criando tela de cadastro de Endereco

--- app/src/main/java/com/example/naytonwilkerson/frontend_integrador_4/MainActivity.java
+++ app/src/main/java/com/example/naytonwilkerson/frontend_integrador_4/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.naytonwilkerson.frontend_integrador_4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void bt2(View view){
+
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(intent);
+    }
+
 }
